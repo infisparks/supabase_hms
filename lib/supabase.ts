863c3eac71a61d4970b93fe087d35332d12b1cb8
infisparks <infisparks@gmail.com>
2025-08-04 21:59:45 +0530

    gg

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://medfordapis.infispark.in/'
+const supabaseUrl = 'https://newmedford.infispark.in'
 const supabaseAnonKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc1NDMyMDc0MCwiZXhwIjo0OTA5OTk0MzQwLCJyb2xlIjoiYW5vbiJ9.cL44o9NQ7iv-aSXmlvae9xKuRtZlpoPfaDF3wuDHkZE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
